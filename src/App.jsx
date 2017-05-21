5fa99ea8cb2a47301682e0aa226b586eb1fe3ab9
Jacques Begin <jacques.andre.begin@gmail.com>
2017-05-21 03:42:12 -0400

    Adds test function for getting an array of messages and displaying them to the chat window upon page load.

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 const uuidV4 = require('uuid/v4');
+const faker = require('faker');
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      username: "Jacques",
+      username: faker.name.findName(),
       userId: uuidV4(),
+      friendname: faker.name.findName(),
+      friendId: uuidV4(),
       messages: []
     }
     this.socket = new WebSocket('ws://localhost:3001/');
@@ -18,7 +21,43 @@ class App extends Component {
   handleAddNewMessage = (message) => {
     message.username = this.state.username;
     message.userId = this.state.userId;
+    message.friendname = this.state.friendname;
+    message.friendId = this.state.friendId;
     this.socket.send(JSON.stringify(message));
+  }
+
+  setMessageStyling = (message) => {
+    if (message.userId != this.state.userId) {
+      message.creator = "chatRightUsername";
+    } else {
+      message.creator = "chatLeftUsername";
+    }
+  }
+
+  loadMessages = () => {
+    const allMessages = [
+      {
+        id: uuidV4(),
+        username: this.state.username,
+        userId: this.state.userId,
+        friendname: this.state.friendname,
+        friendId: this.state.friendId,
+        content: "Hello there!"
+      },
+      {
+        id: uuidV4(),
+        username: this.state.friendname,
+        userId: this.state.friendId,
+        friendname: this.state.username,
+        friendId: this.state.userId,
+        content: "Hello there!"
+      }
+    ]
+    allMessages.forEach((message) => {
+      this.setMessageStyling(message);
+    })
+
+    this.setState({messages: allMessages});
   }
 
   componentDidMount() {
@@ -26,13 +65,11 @@ class App extends Component {
 
     this.socket.onopen = () => {
 
+      this.loadMessages();
+
       this.socket.onmessage = (rawMessage) => {
         let newMessage = JSON.parse(rawMessage.data);
-        if (newMessage.userId != this.state.userId) {
-          newMessage.creator = "chatRightUsername";
-        } else {
-          newMessage.creator = "chatLeftUsername";
-        }
+        this.setMessageStyling(newMessage);
         this.state.messages.push(newMessage);
         this.setState({messages: this.state.messages});
       }
